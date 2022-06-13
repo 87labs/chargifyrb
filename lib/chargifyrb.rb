a56c253898bac12ed35879ca59911f960b62fbf5
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2022-06-13 16:20:49 -0300

    initial commit

---
+++ lib/chargifyrb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "chargifyrb/version"
+
+module Chargifyrb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/chargifyrb_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Chargifyrb do
+  it "has a version number" do
+    expect(Chargifyrb::VERSION).not_to be_nil
+  end
+end
